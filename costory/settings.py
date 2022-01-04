edfa363d815a585232f28501fa929794843d0775
kimhaju <nightskyshop1023@naver.com>
2022-01-04 17:22:40 +0900

    make and test email function

--- costory/settings.py
+++ costory/settings.py
@@ -149,4 +149,9 @@ ACCOUNT_EMAIL_SUBJECT_PREFIX = ""
 
 # Email settings
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "seedgotogether@gmail.com"
+EMAIL_HOST_PASSWORD = "gsyktmztmyezmktt"
